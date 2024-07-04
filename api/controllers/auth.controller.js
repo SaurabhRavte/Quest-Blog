d9ee18ddd7e3d656beaf2072d9202b4b00f77609
Saurabh Ravte <saurabh.ravte@gmail.com>
2024-07-04 23:20:00 +0530

    Missing Bracket

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import errorHandler from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (res, req, next) => {
   const { username, email, password } = req.body;
